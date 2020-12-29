1f4d16310c423e601fd33bf269737b55f237eddb
agenovia <aarongenovia@gmail.com>
2020-12-29 15:14:59 -0800

    adding docstrings and minor fixes

---
+++ qnxt/api/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['AppealAndGrievance', 'Benefit', 'CallTracking', 'Member', 'PlanIntegration', 'Response']
--- qnxt/utils/clean_url.py
+++ qnxt/utils/clean_url.py
@@ -1,6 +1,22 @@
-def clean_url(app_server, base_path):
+def clean_url(app_server, base_path) -> str:
+    """
+    Pass the FQDN of the app server and a base path for the API endpoint, returns the FQDN and base path in the proper
+    string format
+
+    Parameters
+    ----------
+    app_server: str, required
+        FQDN of app server
+    base_path: str, required
+        Base path for the API endpoint
+
+    Returns
+    -------
+    base_url: str
+        Formatted URL string
+    """
     if app_server.endswith('/'):
-        base_uri = f"{app_server[:-1]}{base_path}"
+        base_url = f"{app_server[:-1]}{base_path}"
     else:
-        base_uri = f"{app_server}/{base_path}"
-    return base_uri
+        base_url = f"{app_server}/{base_path}"
+    return base_url
